71379c7fc1020de8112056f58a6c51fb102be860
LegendSohil7 <119963841+LegendSohil7@users.noreply.github.com>
2023-08-29 17:48:58 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+img = "";
+status = "";
+
+function preload() {
+    img = loadImage("desk.jpg")
+}
+
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Lamp", 45, 75);
+    noFill()
+    stroke("#FF0000")
+    rect(30, 60, 250, 350 );
+
+    fill("#031cfc");
+    text("Pencil", 300, 290);
+    noFill()
+    stroke("#031cfc0")
+    circle(330, 375, 130)
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
+
+
+
